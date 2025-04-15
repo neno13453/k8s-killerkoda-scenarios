f8e3f5ed57a135ed11d8aad25fa0911fa60bbf48
Panus Janyapanich <panusjanyapanich@Panuss-MacBook-Pro.local>
2025-04-15 21:49:34 +0700

    test

--- 5-Volumes/nonpersistence1/verify.sh
+++ 5-Volumes/nonpersistence1/verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SIZE=$(kubectl get deploy deploy-with-emptydir -o json | jq '.items[].spec.template.spec.volumes[] | select(.name == "temp-volume") | .emptyDir.sizeLimit')
+SIZE=$(kubectl get deploy deploy-with-emptydir -o json | jq -r '.spec.template.spec.volumes[] | select(.name == "temp-volume") | .emptyDir.sizeLimit')
 
 if [[ $SIZE = "100Mi" ]]; then
     exit 0
