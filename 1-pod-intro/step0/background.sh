7c52a1ab0d44f9124132348c5a898e8dbe5997b1
Panus Janyapanich <panusjanyapanich@Panuss-MacBook-Pro.local>
2025-04-15 16:19:14 +0700

    test

--- 1-pod-intro/step0/background.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl create deploy nginx --image=nginx
-kubectl create secret generic database-secret --from-literal=username=admin --from-literal=password=1234
-kubectl expose deployment nginx --port=80 --target-port=8000
-kubectl create configmap my-config --from-literal=key1=config1 --from-literal=key2=config2
