199931e8de7c4a649f295f9739cc11f01eaf3375
Panus Janyapanich <panusjanyapanich@Panuss-MacBook-Pro.local>
2025-04-27 14:36:47 +0700

    test

--- 4-Configmap-and-Secret/ConfigMap/background.sh
+++ 4-Configmap-and-Secret/ConfigMap/background.sh
@@ -24,14 +24,3 @@ cat << EOF > /tmp/home.html
 </body>
 </html>
 EOF
-
-cat << EOF > /tmp/secret.yaml
-apiVersion: v1
-data:
-  token: YThGYkdrM0xtTjdYcFRxWg==
-kind: Secret
-metadata:
-  name: token
-EOF
-
-openssl req -x509 -newkey rsa:2048 -keyout /root/key.pem -out /root/cert.pem -days 365 -nodes -subj "/CN=localhost"
