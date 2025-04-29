c76dba371ed2cb3a7b907039df7af2f41487f620
Panus Janyapanich <panusjanyapanich@Panuss-MacBook-Pro.local>
2025-04-29 16:43:00 +0700

    test

--- Configmap-and-Secret/Secret/step5/verify.sh
+++ Configmap-and-Secret/Secret/step5/verify.sh
@@ -3,4 +3,4 @@
 kubectl get secret my-secret -oyaml | grep "username: YWRtaW4="
 kubectl get secret my-secret -oyaml | grep "password: MTIzNA=="
 kubectl exec -it backend -- printenv | grep SECRET_USERNAME=admin
-kubectl exec -it backend -- printenv | grep SECRET_PASSWORD=1234 
+kubectl exec -it backend -- printenv | grep SECRET_PASSWORD=1234
